77ed004d7e3df2d907faae5583b20ba3594ffde5
j-m-a-g <jmaghamez@gmail.com>
2023-12-29 14:53:59 -0500

    Add project files.

---
+++ MyMemesWeb/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace MyMemesWeb
+{
+	public partial class MainPage : Page
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+		/*
+		 * GLOBAL DECLARATIONS
+		*/
+		string userNameEmail;
+		string userPassword;
+
+		private void LoginBn_Click(object sender, RoutedEventArgs e)
+		{
+			if (UsernameEmailTtBx.Text == userNameEmail && PasswordPdBx.Password == userPassword)
+			{
+				LoginScrollViewer.Visibility = Visibility.Collapsed;
+			} 
+			else
+			{
+				InvalidCredentialsTtBk.Visibility = Visibility.Visible;
+			}
+		}
+	}
+}
