988cbb7948c1ff748e7a84025c173bed7cf2a027
j-m-a-g <jmaghamez@gmail.com>
2023-12-30 20:28:47 -0500

    Began working on MyMemes application for the web with a drafted login screen

--- MyMemesWeb/MainPage.xaml.cs
+++ MyMemesWeb/MainPage.xaml.cs
@@ -16,16 +16,59 @@ namespace MyMemesWeb
 		string userNameEmail;
 		string userPassword;
 
-		private void LoginBn_Click(object sender, RoutedEventArgs e)
+		/*
+		 * UNDERLYING PROGRAM METHODS
+		*/
+
+		public void LoginScreenPageSwitch(ScrollViewer visibleSlVr)
+		{
+			LoginSlVr.Visibility = Visibility.Collapsed;
+			CreateAccountSlVr.Visibility = Visibility.Collapsed;
+
+			visibleSlVr.Visibility = Visibility.Visible;
+		}
+
+		public void LeaveLoginScreen ()
+		{
+			LoginGd.Visibility = Visibility.Collapsed;
+			LoginSlVr.Visibility = Visibility.Collapsed;
+
+			LandingGd.Visibility = Visibility.Visible;
+		}
+
+		/*
+		 * PROGRAM EVENT HANDLERS
+		 * \\ (Parent container for where the event handlers 
+		 * for the corresponding controls exist)
+		*/
+
+		// LoginSlVr
+		private void LoginHkBn_Click(object sender, RoutedEventArgs e)
 		{
 			if (UsernameEmailTtBx.Text == userNameEmail && PasswordPdBx.Password == userPassword)
 			{
-				LoginScrollViewer.Visibility = Visibility.Collapsed;
+				LoginSlVr.Visibility = Visibility.Collapsed;
 			} 
 			else
 			{
 				InvalidCredentialsTtBk.Visibility = Visibility.Visible;
 			}
+		}
+
+		private void CreateAccountHkBn_Click(object sender, RoutedEventArgs e)
+		{
+			LoginScreenPageSwitch(CreateAccountSlVr);
+		}
+
+		private void NoLoggingInHkBn_Click(object sender, RoutedEventArgs e)
+		{
+			LeaveLoginScreen();
+		}
+
+		// CreateAccountSlVr
+		private void BackToLoginHkBn_Click(object sender, RoutedEventArgs e)
+		{
+			LoginScreenPageSwitch(LoginSlVr);
 		}
 	}
 }
